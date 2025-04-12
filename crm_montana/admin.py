50772923f93a3df642533bd0e54f4e388a842faf
pythonolegy <prostodonald@gmail.com>
2025-04-13 00:01:51 +0300

    added/registered models Department, Employee, updated model Manager

--- crm_montana/admin.py
+++ crm_montana/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Manager
+from .models import Manager, Department, Employee
 
 admin.site.register(Manager)
+admin.site.register(Department)
+admin.site.register(Employee)
--- crm_montana/models.py
+++ crm_montana/models.py
@@ -1,11 +1,34 @@
 from django.db import models
 
+class Department(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
 class Manager(models.Model):
     name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
+    image_url = models.URLField(blank=True, null=True)
     phone = models.CharField(max_length=255)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='managers')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name} {self.last_name}'
+
+class Employee(models.Model):
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    image_url = models.URLField(blank=True, null=True)
+    phone = models.CharField(max_length=24)
+    email = models.EmailField(unique=True)
+    manager=models.ForeignKey(Manager,on_delete=models.CASCADE, related_name='employees')
+    department_pk=models.ForeignKey(Department,on_delete=models.CASCADE, related_name='employees')
+    position = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
